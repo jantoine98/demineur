8ac6caef00cb91ba0fc53786ba9dce78d8e1b6b7
jantoine98 <antoinejarnoux@hotmail.com>
2018-10-27 14:33:44 +0200

    programme principal pour tester les cases

---
+++ src/testJeu/TestCase.java
@@ -0,0 +1,15 @@
+package testJeu;
+
+import demineur.Case;
+
+public class TestCase {
+
+	public static void main(String[] args) 
+	{
+		Case a = new Case(1,2," ");
+		
+		System.out.println(a);
+
+	}
+
+}
