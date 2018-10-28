8493ff1d49a4a968e8559582648bbe757753e269
jantoine98 <antoinejarnoux@hotmail.com>
2018-10-28 18:14:01 +0100

    programme principal pour tester l'affichage d'une grille

---
+++ src/testJeu/TestGrille.java
@@ -0,0 +1,14 @@
+package testJeu;
+
+import demineur.Grille;
+
+public class TestGrille {
+
+	public static void main(String[] args) 
+	{
+		Grille grille = new Grille();
+		System.out.println(grille);
+
+	}
+
+}
