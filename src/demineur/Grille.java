cc2c16662d2f8fc1c0e28dda122fee51239bebab
jantoine98 <antoinejarnoux@hotmail.com>
2018-10-28 18:09:32 +0100

    Fonction qui initialise les cases d'une grille et qui positionne les mines aléatoirement + programme principal pour tester l'affichage d'une grille

--- src/demineur/Grille.java
+++ src/demineur/Grille.java
@@ -1,6 +1,7 @@
 package demineur;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Grille 
 {
@@ -13,6 +14,7 @@ public class Grille
 		cases = new ArrayList<Case>();
 		longueur = 9;
 		largeur = 9;
+		this.initialiserCasesGrille();
 	}
 	
 	public int getLongueur()
@@ -67,22 +69,86 @@ public class Grille
 	}
 	
 	/**
+	 * changer le contenu d'une case de la grille. Cette fonction est appelée à l'initialisation d'une grille
+	 * @param x
+	 * @param y
+	 * @param contenu
+	 */
+	public void setContenuCase(int x,int y, String contenu)
+	{
+		Case caseGrille = this.getCase(x, y);
+		caseGrille.setContenu(contenu);
+	}
+	
+	
+	/**
 	 * afficher les cases d'une grille
 	 */
 	public String toString()
 	{
 		String grilleString = "";
+		
 		for(int i = 0; i < this.longueur; i++)
 		{
-			for(int j = 0; j < this.largeur; i++)
+			for(int j = 0; j < this.largeur; j++)
 			{
-				grilleString += this.getCase(i, j).getContenu();
+				grilleString += this.getCase(i, j).getContenu() + " ";
 			}
 			
 			grilleString += "\n";
 		}
 		
 		return grilleString;
+	}
+	
+	/**
+	 * fonction initialise la grille du jeu
+	 */
+	public void initialiserCasesGrille()
+	{
+		this.initialiserCases();
+		this.positionnerMines();
+	}
+	
+	/**
+	 * cette fonction initialise les cases de la grille avec un contenu vide au début
+	 * cette fonction est privée car elle est juste appelée par la fonction initialiserCases de la classe grille
+	 */
+	private void initialiserCases()
+	{
+		for(int i = 0; i < this.longueur; i++)
+		{
+			for(int j = 0; j < this.largeur;j++)
+			{
+				Case nouvelleCase = new Case(i,j,"_");
+				this.cases.add(nouvelleCase);
+			}
+		}
+	}
+	
+	/**
+	 * cette fonction positionne les mines de façon aléatoire dans la grille 
+	 * cette fonction est privée car elle est juste appelée par la fonction initialiserCases de la classe grille
+	 */
+	private void positionnerMines()
+	{
+		Random r = new Random();
+		
+		for(int i = 0; i < 10; i++)
+		{
+			Case caseMine;
+			int x,y;
+			
+			// faire une boucle do while pour éviter de positionner plus d'une mine dans une même case
+			do
+			{
+				x = r.nextInt(9);
+				y = r.nextInt(9);
+				caseMine = this.getCase(x, y);
+			} while(caseMine.getContenu() == "@");//le @ représente une mine
+			
+			this.setContenuCase(x, y, "@");
+		}
 	}
 
 }
